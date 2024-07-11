6ea046274e4e96bb6ef064245b906338c746c6ec
mlukicdesign <mlukicdesign@gmail.com>
2024-07-11 20:32:37 +0800

    initial commit

---
+++ sanity.query.ts
@@ -0,0 +1,26 @@
+// Construct Main Query for Sanity
+
+import { groq } from "next-sanity";
+import client from "./sanity.client";
+
+
+// async function that returns groq fetch query wrapped with the client config.
+// This function will be used to fetch the profile data from Sanity.
+
+export async function getProfile() {
+    return client.fetch(
+        groq`*[_type == "profile"]{
+          _id,
+          fullName,
+          headline,
+          profileImage {alt, "image": asset->url},
+          shortBio,
+          location,
+          fullBio,
+          email,
+          "resumeURL": resumeURL.asset->url,
+          socialLinks,
+          skills
+        }`
+      );
+    }
---
+++ schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import profile from './profile'
+
+export const schemaTypes = [post, author, category, blockContent, profile]
